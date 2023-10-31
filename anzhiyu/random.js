a36e080533d81a592009d6021361f26fb166606d
yd.zeng <mooez@qq.com>
2023-10-31 22:55:35 +0800

    Site updated: 2023-10-31 22:55:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/10/31/Post”/","2023/10/31/hello-world/"];function toRandomPost(){
+var posts=["2023/10/31/hello-world/","2023/10/31/Post”/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
